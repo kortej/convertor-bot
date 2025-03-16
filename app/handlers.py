a70284bfba3038d69ce90f02ddfa7f7e692c085b
kortej <nazar200792@gmail.com>
2025-03-16 14:44:02 +0200

    Fix func
    Fix func'choose_format'

--- app/handlers.py
+++ app/handlers.py
@@ -1,17 +1,22 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
 import app.keyboards as kb
+from app.keyboards import formats
 
 
 router = Router()
 
+user_format_choise = {}
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(f"Привіт, вибери дію!)", reply_markup=kb.main)
+    await message.answer(f"Привіт, вибери формат для конвертації!: ", reply_markup=kb.format_kb)
 
 
-@router.message(F.text == 'Конвертація')
-async def convert(message: Message):
-    await message.answer(f'Виберіть формат:', reply_markup=kb.format_kb)
+@router.message(F.text.in_(formats))
+async def choose_format(message: Message):
+    user_format_choise[message.from_user.id] = message.text.lower()
+    await message.answer(f"✅ Формат {hbold(message.text)} вибрано. Тепер надішли фото!",
+                        parse_mode="HTML")
